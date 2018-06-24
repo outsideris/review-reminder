7fc3511a7cbe935a20a4cb5ea552bf8856610ade
Outsider <outsideris@gmail.com>
2018-06-24 17:40:09 +0900

    bot fetch review information
    Signed-off-by: Outsider <outsideris@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const createScheduler = require('probot-scheduler');
+const createScheduler = require('probot-scheduler')
+
+const PullRequest = require('./lib/pull-request')
 
 module.exports = robot => {
-  scheduler = createScheduler(robot);
+  const scheduler = createScheduler(robot, {
+    // interval: 60 * 60 * 1000 // 1 hour
+    interval: 1 * 30 * 1000 // 1 hour
+  })
 
-  robot.on('schedule.repository', context => {
-  });
+  robot.on('schedule.repository', notify)
+
+  async function notify (context) {
+    let config = await context.config('review-reminder.yml', {})
+
+    if (!config) {
+      scheduler.stop(context.payload.repository)
+      return
+    }
+
+    const pullRequest = new PullRequest(context.github, context.payload.repository.owner.login, context.payload.repository.name)
+    await pullRequest.opened()
+    await pullRequest.getReviews()
+    await pullRequest.getReviewers()
+  }
 }
